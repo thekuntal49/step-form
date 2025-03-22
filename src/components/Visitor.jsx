c10865b22fe214b50b57e2ca2e0e39e4ee06dad7
Kuntal Kargupta <167575898+thekuntal49@users.noreply.github.com>
2025-03-22 14:41:14 +0530

    Update Visitor.jsx

--- src/components/Visitor.jsx
+++ src/components/Visitor.jsx
@@ -32,7 +32,7 @@ export const Visitor = () => {
         const longitude = position.coords.longitude;
 
         try {
-          await axios.get(`${backendUrl}/api/v1/ip/track`, {
+          await axios.get(`${backendUrl}/api/v1/visit/track`, {
             params: {
               latitude,
               longitude,
@@ -53,7 +53,7 @@ export const Visitor = () => {
           );
           const ip = PROJECT_NAME + "-" + response.data.ip;
 
-          await axios.get(`${backendUrl}/api/v1/ip/track`, {
+          await axios.get(`${backendUrl}/api/v1/visit/track`, {
             params: { ip, deviceId, projectName },
           });
         } catch (error) {
